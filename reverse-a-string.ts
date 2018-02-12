0ba621a396917ab76871d0d53a8b753c9f0ee190
unknown <truongnxt.1512@gmail.com>
2018-02-12 13:29:13 +0700

    Reverse a String

---
+++ reverse-a-string.ts
@@ -0,0 +1,11 @@
+function reverseString (str){
+  reverse = str.split('').reverse().join('');
+  return reverse;
+}
+reverseString('hello');
+
+// function reversString (str: string): string {
+//     let reverse = str.split('').reverse().join('');
+//     return reverse;
+// }
+// reversString('hello');
