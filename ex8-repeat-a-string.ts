9bd62959fa40c10d1c7cd58e59388cca300c55b1
unknown <truongnxt.1512@gmail.com>
2018-02-13 23:28:46 +0700

    1302

---
+++ ex8-repeat-a-string.ts
@@ -0,0 +1,13 @@
+
+function repeatStringNumTimes(str: string, num: number): string {
+    if (num < 0) {
+        return "";
+    }
+    if (num === 1) {
+        return str;
+    }
+    else {
+        return str + repeatStringNumTimes(str, num - 1);
+    }
+}
+repeatStringNumTimes("abc", 3);
